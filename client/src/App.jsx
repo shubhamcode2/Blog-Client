38d56262d4df463e888f443e62ccd127e3449492
shubham singhk <shubhamsinghia160@gmail.com>
2025-05-06 19:05:24 +0530

    check

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,38 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import axios from 'axios';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [totalPosts, setTotalPosts] = useState([])
 
+  const getPosts = async function () {
+
+    axios.get("http://localhost:3000/api/post/get-all-posts?page=2&limit=5")
+      .then(function (response) {
+        console.log(response);
+        setTotalPosts(response.data.posts)
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
+
+  }
   return (
-  <>
-  
-  </>
+    <>
+
+      <h2>get total post here</h2>
+      <button onClick={getPosts}>get</button>
+      <p>Number of Posts</p>
+      {totalPosts.map((item) => {
+        return (
+          <div key={item._id}>
+            {item.content}
+            <img src={item.postPicture} alt="" style={{height:"100px"}}/>
+          </div>
+        )
+      })}
+
+    </>
   )
 }
 
